fd14ab0ad7ffe2aca43d95ab79715a5f1d218c82
Koffi-Sk <kofdzakou1411@gmail.com>
2022-06-07 10:14:34 -0400

    Appel de l'api dans mon  site

---
+++ formations.php
@@ -0,0 +1,34 @@
+<?php
+/*
+json_decode permet de transformer json en tableau
+file_get_contents permet de récupérer les données de l'API (comme un fetch en js)
+
+*/
+$formations = json_decode(file_get_contents("http://dev.h2prog.com/API_TEST/formations"));
+ob_start();
+?>
+
+<table class="table">
+    <tr>
+        <td>Id</td>
+        <td>Nom</td>
+        <td>Description</td>
+        <td>Image</td>
+        <td>Catégorie</td>
+    </tr>
+    <?php foreach ($formations as $formation) : ?>
+    <tr>
+        <td><?= $formation->id ?></td>
+        <td><a href="formation.php?numero=<?= $formation->id ?>"><?= $formation->libelle ?></a></td>
+        <td><?= $formation->description ?></td>
+        <td><img src="<?= $formation->image ?>" width="100px;" /></td>
+        <td><a href="formationsCategorie.php?categorie=<?= $formation->categorie ?>"><?= $formation->categorie ?></a>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+
+
+<?php
+$content = ob_get_clean();
+require_once("template.php");
